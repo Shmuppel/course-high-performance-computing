f98a970bb05d38c5aa285f8e25cf8e798e770e51
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-05-11 17:52:02 +0200

    Add parsing of Phred scores from FastQ file
    Added function to retrieve the Phred scores from FastQ reads, so that
    their values may be averaged.
    
    The function takes every 4th line (containing the Phred scores) from a
    FastQ file, using GNU sed to stream the file, and python subprocess to
    pipe sed's output into a list.
    
    This code is a bit more convoluted than a simple counter loop, but it
    shaves off a couple of seconds of execution time.

--- scripts/average_phred_per_base.py
+++ scripts/average_phred_per_base.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+import os
 import argparse
+import subprocess
 
 
 def parse_command_line():
@@ -8,9 +10,8 @@ def parse_command_line():
     Parses the command line. argparse is used to declare the argument types the script
     requires to run.
 
-    --- Returns ---
-    args (namespace object) : The given arguments by the user parsed into a single
-                              namespace object.
+    :return args (namespace object): The arguments passed by the user parsed into a single
+    namespace object.
     """
     parser = argparse.ArgumentParser(description='Average Phred score per base calculator')
     # Add a positional argument for the FASTQ file path.
@@ -25,8 +26,28 @@ def parse_command_line():
     return args
 
 
+def get_phred_scores(file_path):
+    """
+    Retrieves all lines containing Phred scores from a FastQ file and stores them in a list.
+    Has sed as a dependency to parse every fourth line.
+
+    :param file_path: path pointing to a FastQ file.
+    :return phred_scores: a list containing strings of Phred scores per read.
+    """
+    # Retrieve absolute path in case data is stored in working directory.
+    absolute_file_path = os.path.abspath(file_path)
+
+    # Create a sed process that parses every 4th line, subprocess will pipe the output.
+    get_4th_lines = subprocess.Popen("sed -n '0~4p' " + absolute_file_path, shell=True,
+                                     stdout=subprocess.PIPE, universal_newlines=True)
+    phred_scores = get_4th_lines.stdout.readlines()
+
+    return phred_scores
+
+
 def main():
     args = parse_command_line()
+    phred_scores = get_phred_scores(args.input_fastq[0])
 
 
 if __name__ == "__main__":
