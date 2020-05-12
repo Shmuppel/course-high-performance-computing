26247ac3576581d3e7514972013804865441a469
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-05-13 00:06:04 +0200

    Add output results to csv file given a file path
    Saves the output of the script to a csv file; if a file path is passed
    with the -o argument on the command line.

--- scripts/average_phred_per_base.py
+++ scripts/average_phred_per_base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import csv
 import os
 import sys
 import math
@@ -41,12 +42,12 @@ class AveragePhredCalculator:
 
     def __init__(self, phred_score_lines, no_processes):
         # Start multiprocessing with passed amount of processes.
-        phred_score_matrices = self.multiprocess(no_processes, phred_score_lines)
+        phred_score_matrices = self.multiprocess(phred_score_lines, no_processes)
         # Combine results of processes.
         print("> Combining process results into matrix...")
         self.phred_score_matrix = np.vstack(phred_score_matrices)
 
-    def multiprocess(self, no_processes, phred_score_lines):
+    def multiprocess(self, phred_score_lines, no_processes):
         """
         :param no_processes:
         :param phred_score_lines:
@@ -66,7 +67,7 @@ class AveragePhredCalculator:
             process_no = p + 1
             phred_score_lines_subset = phred_score_lines[chunk_size * p: chunk_size * (p + 1)]
 
-            process = mp.Process(target=self.process_function,
+            process = mp.Process(target=self.__process_function,
                                  args=(process_no,
                                        phred_score_lines_subset,
                                        max_read_length,
@@ -78,32 +79,33 @@ class AveragePhredCalculator:
         # Retrieve output from processes.
         output = [output_queue.get() for p in processes]
 
+        # Wait for processes to complete.
         for p in processes:
             p.join()
 
         return output
 
-    def process_function(self, process_no, phred_score_lines_subset, max_read_length, output_queue):
+    def __process_function(self, process_no, phred_score_lines_subset, max_read_length, output_queue):
         """
-
         :return:
         """
-        print("{process} - Preallocating NumPy matrix subset...".format(process=process_no))
-        phred_score_matrix = self.preallocate_numpy_matrix(phred_score_lines_subset, max_read_length)
+        print("{process} - Preallocating NumPy matrix...".format(process=process_no))
+        phred_score_matrix = self.__preallocate_numpy_matrix(phred_score_lines_subset, max_read_length)
         print("{process} - Filling matrix with numerical Phred scores...".format(process=process_no))
-        phred_score_matrix = self.phred_lines_to_matrix(phred_score_lines_subset, phred_score_matrix)
+        phred_score_matrix = self.__phred_lines_to_matrix(phred_score_lines_subset, phred_score_matrix)
 
         # Add process results to output queue.
         output_queue.put(phred_score_matrix)
 
         return 0
 
-    def preallocate_numpy_matrix(self, phred_score_lines, max_read_length):
+    def __preallocate_numpy_matrix(self, phred_score_lines, max_read_length):
         """
         Preallocate a numpy matrix containing NaN values given a list of Phred score strings.
         Its dimensions are (number of reads/strings, longest read length).
 
-        :param phred_score_lines_count: list of Phred score strings.
+        :param phred_score_lines: list of Phred score strings.
+        :param max_read_length:
         :return phred_score_matrix: numpy matrix filled with NaN values for every possible base location.
         """
         rows = len(phred_score_lines)
@@ -114,7 +116,7 @@ class AveragePhredCalculator:
 
         return phred_score_matrix
 
-    def phred_lines_to_matrix(self, phred_score_lines, phred_score_matrix):
+    def __phred_lines_to_matrix(self, phred_score_lines, phred_score_matrix):
         """
         Iterates over a list of Phred score strings, calculating the ASCII value of its characters
         and placing those values in the Phred score matrix.
@@ -136,7 +138,6 @@ class AveragePhredCalculator:
         """
         Calculates the average value along each Phred score matrix column.
 
-        :param phred_score_matrix: numpy matrix filled with integers (where applicable) or NaN values.
         :return average_phred_per_base: numpy array containing average Phred score per base location.
         """
         print("> Calculating average Phred score per base location...")
@@ -158,7 +159,7 @@ def parse_command_line():
     # Add a positional argument for the FASTQ file path.
     parser.add_argument('input_fastq', help='input FASTQ file path', type=str, nargs=1)
     # Add an optional argument for assigning the number of threads to use.
-    parser.add_argument('-n', '--processes', help='number of processes to assign', type=int, default=2)
+    parser.add_argument('-n', '--processes', help='number of processes to assign', type=int, default=4)
     # Add an optional argument for outputting the results to a CSV file.
     parser.add_argument('-o', '--output', help='output CSV file path', type=str)
 
@@ -174,16 +175,25 @@ def main():
     args = parse_command_line()
 
     # Retrieve absolute path in case data is stored in working directory.
-    file_path = os.path.abspath(args.input_fastq[0])
+    input_file_path = os.path.abspath(args.input_fastq[0])
+    output_file_path = os.path.abspath(args.output)
     no_processes = args.processes
 
     # FastQ file handling.
-    fastq_file_handler = FastQFileHandler(file_path)
+    fastq_file_handler = FastQFileHandler(input_file_path)
     phred_score_lines = fastq_file_handler.get_phred_score_lines()
 
     # Calculate Phred score average per base location using multiprocessing.
     average_phred_calculator = AveragePhredCalculator(phred_score_lines, no_processes)
     average_phred_per_base = average_phred_calculator.calculate_average_phred_per_base()
+
+    # If output path is given, save results to file.
+    if output_file_path is not None:
+        print("> Saving results to: {output}".format(output=output_file_path))
+        with open(output_file_path, 'w') as output_file:
+            writer = csv.writer(output_file, delimiter=',')
+            writer.writerow(['base index', 'average phred score'])
+            writer.writerows([index + 1, average] for index, average in enumerate(average_phred_per_base))
 
     return 0
 
