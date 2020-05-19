267e3fbbc4edab354540668166070afc5f04895a
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-05-19 22:12:27 +0200

    Make script fit for star topology style excution
    Rewrites most of the classes to better suit the server - client -
    process workflow. This version is nonfunctional and contains a fair
    amount of unfinished and unadapted code.

--- scripts/cluster_average_phred_per_base.py
+++ scripts/cluster_average_phred_per_base.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 """
-Description: Script that calculates the average Phred score per base location of a FastQ file.
-Usage: average_phred_per_base.py <FastQ file path> [-n <number of processes to use>] [-o CSV output file path]
-Author: Niels van der Vegt (#365285)
 Dependencies:
     - numpy
     - sed (UNIX)
 """
 
 import os
+import queue
 import sys
-import csv
+import time
+from multiprocessing.managers import BaseManager
 import math
 import argparse
 import subprocess
@@ -18,119 +17,33 @@ import numpy as np
 import multiprocessing as mp
 
 
-class FastQFileHandler:
-
-    def __init__(self, file_path):
-        """
-        Object containing methods for handling FastQ files.
-
-        :param file_path: file path to the FastQ file to be used.
-        """
-        self.file_path = file_path
-
-    def get_phred_score_lines(self):
-        """
-        Retrieves all lines containing Phred scores from a FastQ file and stores them in a list.
-        Has sed as a dependency to parse every fourth line.
-
-        :return phred_scores: a list containing strings of Phred scores per read.
-        """
-        print("> Retrieving Phred score lines from FastQ file...")
-        # Create a sed process that parses every 4th line (0~4p).
-        get_4th_lines = subprocess.Popen("sed -n '0~4p' " + self.file_path,
-                                         shell=True,
-                                         stdout=subprocess.PIPE,
-                                         universal_newlines=True)
-
-        # Subprocess will pipe the output, lines are stripped of whitespace / newline characters.
-        phred_score_lines = [line.strip() for line in get_4th_lines.stdout.readlines()]
-
-        return phred_score_lines
-
-
 class AveragePhredCalculator:
 
-    def __init__(self, phred_score_lines, no_processes):
-        """
-        Object containing methods using multiprocessing to calculate the average phred per base location,
-        given a list of Phred score strings and the no_processes to use. Initializes a Phred score matrix
-        with integer values representing every Phred score character in every Phred score line.
-
-        :param phred_score_lines: list of Phred score strings to calculate average Phred across.
-        :param no_processes: number of process to assign calculations.
-        """
-        phred_score_matrices = self.start_processes(phred_score_lines, no_processes)
-        print("> Combining process results into matrix...")
-        self.phred_score_matrix = np.vstack(phred_score_matrices)
-
-    def start_processes(self, phred_score_lines, no_processes):
-        """
-        Starts a specified number of processes, with each process receiving a subset of Phred score
-        string to perform conversion to ASCII values upon.
-
-        :param phred_score_lines: list of Phred score strings.
-        :param no_processes: number of process to assign calculations.
-        :return output: list of NumPy matrices containing each process' results.
-        """
-        # Initialize multiprocessing variables.
-        processes = []
-        output_queue = mp.Queue()
-
-        # Initialize data separation related variables.
-        phred_score_lines_count = len(phred_score_lines)
-        max_read_length = len(max(phred_score_lines, key=len))
-        chunk_size = int(math.ceil(phred_score_lines_count / no_processes))
-
-        print("> Starting {n} processes...".format(n=no_processes))
-        for p in range(no_processes):
-            process_no = p + 1
-            phred_score_lines_subset = phred_score_lines[chunk_size * p: chunk_size * (p + 1)]
-
-            process = mp.Process(target=self.__process_function,
-                                 args=(process_no,
-                                       phred_score_lines_subset,
-                                       max_read_length,
-                                       output_queue))
-
-            processes.append(process)
-            process.start()
-
-        # Retrieve output from processes.
-        output = [output_queue.get() for p in processes]
-
-        # Wait for processes to complete.
-        for p in processes:
-            p.join()
-
-        return output
-
-    def __process_function(self, process_no, phred_score_lines_subset, max_read_length, output_queue):
+    def __init__(self, process_no, phred_score_lines_subset, output_queue):
         """
         Worker function to be called for every process. Given a subset of Phred score lines to process,
         allocate a NumPy matrix and fill it with the strings' character ASCII values. Places resulting
         NumPy matrix in the shared queue when finished.
         """
         print("{process} - Preallocating NumPy matrix...".format(process=process_no))
-        phred_score_matrix = self.__preallocate_numpy_matrix(phred_score_lines_subset, max_read_length)
+        phred_score_matrix = self.__preallocate_numpy_matrix(phred_score_lines_subset)
         print("{process} - Filling matrix with numerical Phred scores...".format(process=process_no))
         phred_score_matrix = self.__phred_lines_to_matrix(phred_score_lines_subset, phred_score_matrix)
+        # TODO - Average results, send those results back
 
         # Add process results to output queue.
         output_queue.put(phred_score_matrix)
 
-        return 0
-
-    def __preallocate_numpy_matrix(self, phred_score_lines, max_read_length):
+    def __preallocate_numpy_matrix(self, phred_score_lines):
         """
         Preallocate a numpy matrix containing NaN values given a list of Phred score strings.
         Its dimensions are (number of phred strings in subset, overall longest read length).
 
         :param phred_score_lines: list of Phred score strings.
-        :param max_read_length: the longest read length found across all processes.
         :return phred_score_matrix: numpy matrix filled with NaN values for every possible base location.
         """
         rows = len(phred_score_lines)
-        columns = max_read_length
+        columns = len(max(phred_score_lines, key=len))
 
         # Using data-type 8-bit unsigned integer (u1) to save memory (Phred scores can't be negative nor > 104).
         phred_score_matrix = np.full(shape=(rows, columns),
@@ -154,7 +67,7 @@ class AveragePhredCalculator:
 
         return phred_score_matrix
 
-    def calculate_average_phred_per_base(self):
+    def __calculate_average_phred_per_base(self, phred_score_matrix):
         """
         Calculates the average value along each Phred score matrix column.
 
@@ -162,9 +75,143 @@ class AveragePhredCalculator:
         """
         print("> Calculating average Phred score per base location...")
         # matrix.nanmean to support the trailing NaN values for bases shorter than the longest read.
-        average_phred_per_base = np.nanmean(self.phred_score_matrix, axis=0)
+        average_phred_per_base = np.nanmean(phred_score_matrix, axis=0)
 
         return average_phred_per_base
+
+
+class Client:
+
+    def __init__(self, file_path, no_workers):
+        """
+        Object containing methods using multiprocessing to calculate the average phred per base location,
+        given a list of Phred score strings and the no_processes to use. Initializes a Phred score matrix
+        with integer values representing every Phred score character in every Phred score line.
+
+        :param no_workers: number of process to assign calculations.
+        """
+        phred_score_lines = self.get_phred_score_lines(file_path)
+        phred_score_matrices = self.run_workers(phred_score_lines, no_workers)
+        print(phred_score_matrices)
+
+    def get_phred_score_lines(self, file_path):
+        """
+        Retrieves all lines containing Phred scores from a FastQ file and stores them in a list.
+        Has sed as a dependency to parse every fourth line.
+
+        :return phred_scores: a list containing strings of Phred scores per read.
+        """
+        print("> Retrieving Phred score lines from FastQ file...")
+        # Create a sed process that parses every 4th line (0~4p).
+        get_4th_lines = subprocess.Popen("sed -n '0~4p' " + file_path,
+                                         shell=True,
+                                         stdout=subprocess.PIPE,
+                                         universal_newlines=True)
+
+        # Subprocess will pipe the output, lines are stripped of whitespace / newline characters.
+        phred_score_lines = [line.strip() for line in get_4th_lines.stdout.readlines()]
+
+        return phred_score_lines
+
+    def run_workers(self, phred_score_lines, no_processes):
+        """
+        Starts a specified number of processes, with each process receiving a subset of Phred score
+        string to perform conversion to ASCII values upon.
+
+        :param phred_score_lines: list of Phred score strings.
+        :param no_processes: number of process to assign calculations.
+        :return output: list of NumPy matrices containing each process' results.
+        """
+        # Initialize multiprocessing variables.
+        processes = []
+        output_queue = mp.Queue()
+
+        # Initialize data separation related variables.
+        phred_score_lines_count = len(phred_score_lines)
+        chunk_size = int(math.ceil(phred_score_lines_count / no_processes))
+
+        print("> Starting {n} processes...".format(n=no_processes))
+        for p in range(no_processes):
+            process_no = p + 1
+            phred_score_lines_subset = phred_score_lines[chunk_size * p: chunk_size * (p + 1)]
+
+            process = mp.Process(target=AveragePhredCalculator,
+                                 args=(process_no,
+                                       phred_score_lines_subset,
+                                       output_queue))
+
+            processes.append(process)
+            process.start()
+
+        # Retrieve output from processes.
+        output = [output_queue.get() for p in processes]
+
+        # Wait for processes to complete.
+        for p in processes:
+            p.join()
+
+        return output
+
+
+class Server:
+
+    def __init__(self, function, file_path, port):
+        """
+
+        """
+        self.file_path = file_path
+        self.function = function
+
+        self.manager = self.initialize_server_manager(port, b'authentication')
+        self.shared_job_queue = self.manager.get_job_queue()
+        self.shared_result_queue = self.manager.get_result_queue()
+
+    def initialize_server_manager(self, port, authkey):
+        """
+        :param port:
+        :param authkey:
+        :return:
+        """
+        job_queue = queue.Queue()
+        result_queue = queue.Queue()
+
+        class ServerManager(BaseManager):
+            pass
+
+        ServerManager.register('get_job_queue', callable=lambda: job_queue)
+        ServerManager.register('get_result_queue', callable=lambda: result_queue)
+
+        manager = ServerManager(address=('', port), authkey=authkey)
+        manager.start()
+
+        return manager
+
+    def run_server(self):
+        """
+        :return:
+        """
+        for item in self.data:
+            self.shared_job_queue.put({'function': self.function, 'data': item})
+
+        time.sleep(2)
+
+        results = []
+        while True:
+            try:
+                result = self.shared_result_queue.get_nowait()
+                results.append(result)
+
+                if len(results) == len(self.data):
+                    break
+
+            except queue.Empty:
+                time.sleep(1)
+                continue
+
+        self.shared_job_queue.put("KILL")
+        time.sleep(5)
+        self.manager.shutdown()
+        print(results)
 
 
 def parse_command_line():
@@ -174,35 +221,19 @@ def parse_command_line():
     :return args: The arguments passed by the user parsed into a single namespace object.
     """
     parser = argparse.ArgumentParser(description='Average Phred score per base calculator')
-    parser.add_argument('input_fastq', help='input FASTQ file path', type=str, nargs=1)
-    parser.add_argument('-n', '--processes', help='number of processes to assign', type=int, default=4)
+    parser.add_argument('-f', '--input_fastq', help='input FASTQ file path', type=str, required=True)
     parser.add_argument('-o', '--output', help='output CSV file path', type=str)
+    parser.add_argument('-n', '--processes', help='number of processes to assign per client', type=int, default=4)
+    parser.add_argument('-p', '--port', help='port to use for server-host communication', type=int, required=True)
+    parser.add_argument('--hosts', help='hosts, first host should refer to the server', type=str, required=True)
+
+    execution_type = parser.add_mutually_exclusive_group(required=True)
+    execution_type.add_argument('-c', '--client', help='run script as client', action='store_false')
+    execution_type.add_argument('-s', '--server',  help='run script as server', action='store_true')
 
     args = parser.parse_args()
 
     return args
-
-
-def output_results_to_csv(output_file_path, average_phred_per_base):
-    """
-    Outputs the average Phred score per base to a given CSV file path.
-
-    :param output_file_path: output file path to generate CSV file at.
-    :param average_phred_per_base: numpy array containing the average phred per base location.
-    """
-    print("> Saving results to: {output}".format(output=output_file_path))
-    try:
-        with open(output_file_path, 'w') as output_file:
-            writer = csv.writer(output_file, delimiter=',')
-            writer.writerow(['base index', 'average phred score'])
-
-            # Write average per base index, round average to 3 decimals.
-            writer.writerows([index + 1, round(average, 3)]
-                             for index, average in enumerate(average_phred_per_base))
-
-    except EnvironmentError:
-        print("Could not create output file, please check if the given output file path is valid.")
-        sys.exit(1)
 
 
 def main():
@@ -212,27 +243,24 @@ def main():
     :return: 0 when the program finishes correctly.
     """
     args = parse_command_line()
-
-    # Retrieve absolute path in case data is stored in working directory.
     input_file_path = os.path.abspath(args.input_fastq[0])
     no_processes = args.processes
+    port = args.port
+    hosts = args.hosts
+    execution_type = args.server
 
-    # FastQ file handling.
-    fastq_file_handler = FastQFileHandler(input_file_path)
-    phred_score_lines = fastq_file_handler.get_phred_score_lines()
-
-    # Calculate Phred score average per base location using multiprocessing.
-    average_phred_calculator = AveragePhredCalculator(phred_score_lines, no_processes)
-    average_phred_per_base = average_phred_calculator.calculate_average_phred_per_base()
-
-    # If output path is given, save results to file.
-    if args.output is not None:
-        output_file_path = os.path.abspath(args.output)
-        output_results_to_csv(output_file_path, average_phred_per_base)
+    if execution_type:
+        #execute server
+        server = mp.Process(target=Server, args=(AveragePhredCalculator, input_file_path))
+        #server.start()
+        #server.join()
+        pass
     else:
-        print(np.round(average_phred_per_base, decimals=2))
-
-    return 0
+        #execute client
+        server = mp.Process(target=Client, args=(no_processes))
+        #client.start()
+        #client.join()
+        pass
 
 
 if __name__ == "__main__":
