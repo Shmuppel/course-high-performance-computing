ae4dcc5ed91e9c3a4232e2a388432271129cf79a
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-05-30 05:11:21 +0200

    Add remote client startup
    Adds remote client startup for clients passed with
    the --hosts argument.

--- scripts/cluster_average_phred_per_base.py
+++ scripts/cluster_average_phred_per_base.py
@@ -4,7 +4,7 @@ Dependencies:
     - numpy
     - sed (UNIX)
 """
-
+import csv
 import os
 import queue
 import sys
@@ -19,21 +19,21 @@ import multiprocessing as mp
 
 class AveragePhredCalculator:
 
-    def __init__(self, process_no, phred_score_lines_subset, output_queue):
+    def __init__(self, process_no, phred_score_lines_subset, max_read_length, output_queue):
         """
         Worker function to be called for every process. Given a subset of Phred score lines to process,
         allocate a NumPy matrix and fill it with the strings' character ASCII values. Places resulting
         NumPy matrix in the shared queue when finished.
         """
         print("{process} - Preallocating NumPy matrix...".format(process=process_no))
-        phred_score_matrix = self.__preallocate_numpy_matrix(phred_score_lines_subset)
+        phred_score_matrix = self.__preallocate_numpy_matrix(phred_score_lines_subset, max_read_length)
         print("{process} - Filling matrix with numerical Phred scores...".format(process=process_no))
         phred_score_matrix = self.__phred_lines_to_matrix(phred_score_lines_subset, phred_score_matrix)
         # TODO: Cleanup average declaration
         # Add process average results to output queue.
         output_queue.put(self.__calculate_average_phred_per_base(phred_score_matrix))
 
-    def __preallocate_numpy_matrix(self, phred_score_lines):
+    def __preallocate_numpy_matrix(self, phred_score_lines, max_read_length):
         """
         Preallocate a numpy matrix containing NaN values given a list of Phred score strings.
         Its dimensions are (number of phred strings in subset, overall longest read length).
@@ -42,7 +42,7 @@ class AveragePhredCalculator:
         :return phred_score_matrix: numpy matrix filled with NaN values for every possible base location.
         """
         rows = len(phred_score_lines)
-        columns = len(max(phred_score_lines, key=len))
+        columns = max_read_length
 
         # Using data-type 8-bit unsigned integer (u1) to save memory (Phred scores can't be negative nor > 104).
         phred_score_matrix = np.full(shape=(rows, columns),
@@ -105,8 +105,8 @@ class Client:
         print("> Retrieving Phred score lines from FastQ file...")
         # Create a sed process that parses every 4th line (0~4p).
         get_4th_lines = subprocess.Popen("sed -n '{start}~4p;{stop}q' {file_path}".format(start=start,
-                                                                                         stop=stop,
-                                                                                         file_path=file_path),
+                                                                                          stop=stop,
+                                                                                          file_path=file_path),
                                          shell=True,
                                          stdout=subprocess.PIPE,
                                          universal_newlines=True)
@@ -123,6 +123,7 @@ class Client:
         :param authkey:
         :return:
         """
+
         class ClientManager(BaseManager):
             pass
 
@@ -154,14 +155,15 @@ class Client:
                 else:
                     function = job['function']
                     chunk_indices = job['chunk_indices']
+
                     phred_score_lines = self.get_phred_score_lines(file_path, chunk_indices[0], chunk_indices[1])
+
                     results = self.__start_processes(phred_score_lines, no_processes)
                     result_queue.put(results)
 
             except queue.Empty:
                 print("sleep")
                 time.sleep(1)
-
 
     def __start_processes(self, phred_score_lines, no_processes):
         """
@@ -187,12 +189,13 @@ class Client:
             process = mp.Process(target=AveragePhredCalculator,
                                  args=(process_no,
                                        phred_score_lines_subset,
+                                       max_read_length,
                                        output_queue))
 
             processes.append(process)
             process.start()
 
-        # Retrieve output from processes.
+        # Retrieve output from processes and average it.
         output = np.vstack([output_queue.get() for p in processes])
         output = np.sum(output, axis=0) / no_processes
 
@@ -215,18 +218,17 @@ class Server:
         self.file_path = file_path
         self.function = function
         self.port = port
+        self.no_clients = no_clients
 
         self.chunks = self.get_chunk_indices(no_clients)
         self.manager = self.__initialize_server_manager(port, authkey)
-
-        self.__run_server()
 
     def get_file_length(self):
         """
         :return:
         """
         output = subprocess.run(['wc', '-l', self.file_path], capture_output=True, text=True)
-        # get first element from wc -l output (lines in file)
+        # get first element from wc -l output (line count of file)
         output = int(output.stdout.split()[0])
 
         return output
@@ -244,7 +246,7 @@ class Server:
 
         for client in range(no_clients):
             chunks.append((chunk_size * client,
-                           chunk_size * (client + 1)))
+                           chunk_size * (client + 1) - 1))
 
         return chunks
 
@@ -268,7 +270,7 @@ class Server:
 
         return manager
 
-    def __run_server(self):
+    def run_server(self):
         """
         :return:
         """
@@ -277,7 +279,6 @@ class Server:
 
         for chunk_indices in self.chunks:
             shared_job_queue.put({'function': self.function, 'chunk_indices': chunk_indices})
-            print(chunk_indices)
 
         time.sleep(5)
 
@@ -294,13 +295,63 @@ class Server:
                 time.sleep(1)
                 continue
 
-        # TODO: calculate average result (/ no clients)
+        # TODO - Clean this up
+        concatenated_results = np.full(shape=(len(results), max([len(result) for result in results])),
+                                       fill_value=np.NaN)
+        for i, result in enumerate(results):
+            concatenated_results[i, 0:len(result)] = result
+
+        output = np.sum(concatenated_results, axis=0) / self.no_clients
 
         shared_job_queue.put("KILL")
         time.sleep(5)
 
         self.manager.shutdown()
-        print(results)
+
+        return output
+
+
+def output_results_to_csv(output_file_path, average_phred_per_base):
+    """
+    Outputs the average Phred score per base to a given CSV file path.
+
+    :param output_file_path: output file path to generate CSV file at.
+    :param average_phred_per_base: numpy array containing the average phred per base location.
+    """
+    print("> Saving results to: {output}".format(output=output_file_path))
+    try:
+        with open(output_file_path, 'w') as output_file:
+            writer = csv.writer(output_file, delimiter=',')
+            writer.writerow(['base index', 'average phred score'])
+
+            # Write average per base index, round average to 3 decimals.
+            writer.writerows([index + 1, round(average, 3)]
+                             for index, average in enumerate(average_phred_per_base))
+
+    except EnvironmentError:
+        print("Could not create output file, please check if the given output file path is valid.")
+        sys.exit(1)
+
+
+def initiate_clients(client_list, args):
+    """
+    :param client_list:
+    :param server_ip:
+    :return:
+    """
+    script_path = os.path.realpath(__file__)
+
+    for client in client_list:
+        subprocess.Popen("ssh {client} " \
+                         "{script_path} -f {input_file} " \
+                         "-n {processes} -p {port} --hosts {server_ip} -c"
+                         .format(client=client,
+                                 script_path=script_path,
+                                 input_file=args.input_fastq[0],
+                                 processes=args.processes,
+                                 port=args.port,
+                                 server_ip=args.hosts[0]),
+                         shell=True)
 
 
 def parse_command_line():
@@ -312,7 +363,7 @@ def parse_command_line():
     parser.add_argument('-o', '--output', help='output CSV file path', type=str)
     parser.add_argument('-n', '--processes', help='number of processes to assign per client', type=int, default=4)
     parser.add_argument('-p', '--port', help='port to use for server-host communication', type=int, required=True)
-    parser.add_argument('--host', help='server ip', type=str, required=True, nargs='+')
+    parser.add_argument('--hosts', help='server and client ips respectively', type=str, required=True, nargs='+')
 
     execution_type = parser.add_mutually_exclusive_group(required=True)
     execution_type.add_argument('-c', '--client', help='run script as client', action='store_false')
@@ -334,22 +385,36 @@ def main():
     # get absolute filepath in case a relative path is given.
     input_file_path = os.path.abspath(args.input_fastq[0])
     no_processes = args.processes
-    port = args.port
-    host = args.host
-    execution_type = args.server
-    authkey = b'authentication'
 
+    port = args.port
+    hosts = args.hosts
+    authkey = b'authentication'
+    execution_type = args.server
+
+    # server & client arguments set to true if server, false if client.
     if execution_type:
-        # execute server
-        # TODO change host
-        server = Server(input_file_path, AveragePhredCalculator, len(host) - 1, port, authkey)
+        server = Server(input_file_path, AveragePhredCalculator, len(hosts) - 1, port, authkey)
+
+        # launch remote clients
+        if len(hosts) > 1:
+            client_list = hosts[1:]
+            initiate_clients(client_list, args)
+
+        results = server.run_server()
+
+        # output results to file if -o argument is passed.
+        if args.output is not None:
+            output_file_path = os.path.abspath(args.output)
+            output_results_to_csv(output_file_path, results)
+        else:
+            print(np.round(results, decimals=2))
+
     else:
-        # TODO server ip to actual server
-        server_ip = 'bin201'
-        # execute client
+        server_ip = hosts[0]
         client = Client(input_file_path, no_processes, server_ip, port, authkey)
 
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
